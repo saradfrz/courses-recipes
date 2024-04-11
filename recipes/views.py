8374dcd7434f65110bc13048d455616cdd0213d8
Sara Fernandez <saradfrz@gmail.com>
2024-04-11 06:21:29 -0300

    L28: Create the head.html partial

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Eirwynn Mistwalker',
     })
