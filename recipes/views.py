e857db4d49a6087a442bfe0ef284f482d344722c
Sara Fernandez <saradfrz@gmail.com>
2024-04-11 05:33:12 -0300

    L.24: Templates and settings.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('HOME 2')
-
+    return render(request, 'global/home.html')
 
 def contato(request):
     return HttpResponse('contato')
-
-
 def sobre(request):
     return HttpResponse('sobre')
--- recipes_project/settings.py
+++ recipes_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipes'
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,9 @@ ROOT_URLCONF = 'recipes_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
