3fcedcd30d3fed93f5bf7fa23a94c087eb884824
Sara Fernandez <saradfrz@gmail.com>
2024-04-11 04:47:24 -0300

    L.22: Creating an app

--- recipes_project/urls.py
+++ recipes_project/urls.py
@@ -18,11 +18,22 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
 
-# Criar uma view
-def my_view(request):
-    return HttpResponse('UM LINDA STRING')
+# Criar views
+def home(request):
+    return HttpResponse('HOME')
+
+
+def contato(request):
+    return HttpResponse('contato')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', my_view),
+    path('', home),  # Home
+    path('sobre/', sobre),  # /sobre/
+    path('contato/', contato),  # /contato/
 ]
