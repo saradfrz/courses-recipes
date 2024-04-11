22268c5d767ba5276fbcf12d23071b5364323a92
Sara Fernandez <saradfrz@gmail.com>
2024-04-11 05:02:11 -0300

    L.23: Move code to the app recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('HOME 2')
+
+
+def contato(request):
+    return HttpResponse('contato')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
