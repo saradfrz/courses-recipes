22e3f171bd5fe7f95c3aa89f7ba48dd50f105572
Sara Fernandez <saradfrz@gmail.com>
2024-04-22 16:07:09 -0300

    L.42 - New view

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Eirwynn Mistwalker',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio',
+    })
