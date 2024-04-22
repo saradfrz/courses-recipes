737938356152ee031693475c9263067d95390549
Sara Fernandez <saradfrz@gmail.com>
2024-04-22 13:47:42 -0300

    L.40 - STATIC_ROOT

--- recipes_project/settings.py
+++ recipes_project/settings.py
@@ -128,6 +128,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+STATIC_ROOT = BASE_DIR / 'static'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
