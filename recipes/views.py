f9d0aa6ee90330c9163ca6053c0f00aad1c904b8
Sara Fernandez <saradfrz@gmail.com>
2024-04-25 09:07:05 -0300

    L.48 - 48. Trabalhando nas diferenças entre detail page e list page

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,15 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
+
 
 def home(request):
     return render(request, 'global/home.html', context={
-        'name': 'Eirwynn Mistwalker',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Luiz Otávio',
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
