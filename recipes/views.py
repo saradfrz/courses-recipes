af3ceb4bf454ecf01037fbffdf2559920e5c6301
Sara Fernandez <saradfrz@gmail.com>
2024-04-27 16:30:53 -0300

    L.59 - Ocultando receitas que não estão publicadas

--- recipes/views.py
+++ recipes/views.py
@@ -6,7 +6,9 @@ from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -14,15 +16,20 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id
+                category__id=category_id,
+        is_published=True,
     ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/category.html', context={
     'recipes': recipes,
     })
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+                id=id,
+        is_published=True,
+    ).first()
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
